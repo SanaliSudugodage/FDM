459551802ea48866eca548b0ab308c62b449a108
SanaliSudugodage <IT22557742@my.sliit.lk>
2025-10-03 20:32:57 +0530

    Update app.py

--- FDM Project Files/app.py
+++ FDM Project Files/app.py
@@ -86,6 +86,7 @@ if st.button('Predict Booking Cancellation'):
     result = predict_booking(features)
     st.success(f"The booking is predicted to be: {result}")
 
+####################################################################
 
 
 
