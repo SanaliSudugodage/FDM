ac414ab60ab52929f10bb60f0e30abc0abb25d9b
SanaliSudugodage <IT22557742@my.sliit.lk>
2025-10-03 20:21:06 +0530

    Update app.py

--- FDM Project Files/app.py
+++ FDM Project Files/app.py
@@ -82,7 +82,4 @@ features = [
     market_segment_dict[market_segment_type]  # Encoded value of Market Segment
 ]
 
-# Display prediction button
-if st.button('Predict Booking Cancellation'):
-    result = predict_booking(features)
-    st.success(f"The booking is predicted to be: {result}")
+
