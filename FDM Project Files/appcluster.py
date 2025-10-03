200b66044d0b91e419a41d9218bbd1f0b1bc55be
SanaliSudugodage <IT22557742@my.sliit.lk>
2025-10-03 20:37:45 +0530

    Update appcluster.py

--- FDM Project Files/appcluster.py
+++ FDM Project Files/appcluster.py
@@ -38,9 +38,7 @@ if st.button('Predict Cluster'):
     else:
         cluster_label = 'Frequent'
 
-    # Display the predicted cluster
-    st.write(f'The predicted customer cluster is: **{cluster_label}**')
-
+    
 
 
 
