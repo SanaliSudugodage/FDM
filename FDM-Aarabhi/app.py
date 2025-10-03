edb965fbb71f9a7cdf503a35f61a7fb18339956c
Aarabhi Anton Yoganayagam <156441711+aarabhi12@users.noreply.github.com>
2025-10-03 20:45:53 +0530

    Update app.py

--- FDM-Aarabhi/app.py
+++ FDM-Aarabhi/app.py
@@ -85,6 +85,6 @@ features = [
 # Display prediction button
 if st.button('Predict Booking Cancellation'):
     result = predict_booking(features)
-    st.success(f"The booking is predicted to be: {result}")
+    st.success(f"The booking is predicted to be: {result}"
 
 
